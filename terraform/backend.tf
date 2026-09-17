10d71f8c9ed23aa76b6a860e1d262317a46e40b8
Emile Deloof <emile.deloof@student.hogent.be>
2026-09-17 21:11:58 +0200

    Add terraform state to R2 bucket

---
+++ terraform/backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket   = "homelab"
+    key      = "production/tofu.tfstate"
+    region   = "auto"
+    endpoint = "https://e7ee5ef2f236a9da74f47c10431d23e7.r2.cloudflarestorage.com"
+
+    # Required flags for Cloudflare R2 / S3 API compatibility
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+  }
+}
